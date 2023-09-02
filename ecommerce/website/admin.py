9e968b5662889295463e22b5419e715a4c2f0936
Mahmoud Fathy <58611801+ZeroInverted@users.noreply.github.com>
2023-09-02 21:39:53 +0300

    Update auth_user_model and admin.py registrations

--- ecommerce/website/admin.py
+++ ecommerce/website/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Admin, Customer
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
-admin.site.register(Admin)
+admin.site.register(Admin, UserAdmin)
 admin.site.register(Customer)
