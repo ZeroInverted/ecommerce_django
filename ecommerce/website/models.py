6d327832d7244f8f29385866578393dfbce78658
Mahmoud Fathy <58611801+ZeroInverted@users.noreply.github.com>
2023-09-02 20:02:20 +0300

    Update models based on class diagram

--- ecommerce/website/models.py
+++ ecommerce/website/models.py
@@ -7,7 +7,7 @@ class User(models.Model):
     full_name = models.CharField(max_length=200)
     email = models.EmailField(max_length=250)
     password = models.CharField(max_length=100)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
     class Meta:
         abstract = True
@@ -21,7 +21,7 @@ class Admin(User):
 
 class Customer(User):
     phone = models.CharField(max_length=30)
-    adress = models.CharField(max_length=200)
+    adress = models.TextField()
 
 
 class Category(models.Model):
@@ -31,24 +31,27 @@ class Category(models.Model):
 class Product(models.Model):
     name_en = models.CharField(max_length=200)
     name_ar = models.CharField(max_length=200)
-    unit_price = models.DecimalField(max_digits=10)
-    stock_quantity = models.DecimalField(max_digits=5)
-    category = models.ManyToManyField(Category, on_delete=models.RESTRICT)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock_quantity = models.PositiveIntegerField()
+    category_id = models.ForeignKey(Category, on_delete=models.RESTRICT)
 
 
 class Order(models.Model):
     create_date = models.DateTimeField(default=datetime.now())
-    total_price = models.DecimalField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
     customer_id = models.ForeignKey(Customer, on_delete=models.RESTRICT)
-    # todo: status enum
-
+    status_choices = {
+        ('Pending', 'Pending'),
+        ('Approved', 'Approved'),
+        ('Declined', 'Declined'),
+        ('Shipped', 'Shipped'),
+        ('Delivered', 'Delivered')    
+    }
+    status = models.CharField(max_length=20, choices=status_choices)
 
 class OrderDetails(models.Model):
     order_id = models.ForeignKey(Order, on_delete=models.RESTRICT)
-    product_id = models.ManyToManyField(Product, on_delete=models.RESTRICT)
-    unit_price = models.DecimalField(max_digits=6)
-    ordered_count = models.IntegerField(default=0)
+    product_id = models.ForeignKey(Product, on_delete=models.RESTRICT)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    ordered_count = models.PositiveIntegerField()
 
-    def save(self, *args, **kwargs):
-        # todo: increase order counter dynamically
-        pass
