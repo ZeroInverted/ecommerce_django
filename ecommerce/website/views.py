592eb9a9cbe44c8b9b89ccae6068bf0ab1271079
Mahmoud Fathy <58611801+ZeroInverted@users.noreply.github.com>
2023-09-06 21:00:11 +0300

    Update path to include website app path, add template with base template inheritance

--- ecommerce/website/views.py
+++ ecommerce/website/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
+
+
+class Home(View):
+    def get(self, request):
+        return render(request, "website/index.html")
+
+
+class Register(View):
+    def get(self, request):
+        return render(request, "website/register.html")
+
+
+class Login(View):
+    def get(self, request):
+        return render(request, "website/login.html")
+
+
+class Logout(View):
+    def get(self, request):
+        pass
